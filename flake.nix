8ebbd8b490db01273974865529b766bc1cefe9ba
niten <niten@fudo.org>
2024-07-13 21:04:44 -0700

    Update deps

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Immich running in a container";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-23.11";
+    nixpkgs.url = "nixpkgs/nixos-24.05";
     arion.url = "github:hercules-ci/arion";
   };
 
